0dd1d766797371de9f0856148c8f5981f2d84bf9
dongja94 <dongja94@gmail.com>
2016-01-14 17:56:56 +0900

    More Data

--- app/src/main/java/com/example/dongja94/samplecustomchoicelist/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplecustomchoicelist/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dongja94.samplecustomchoicelist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,10 +20,37 @@ public class MainActivity extends AppCompatActivity {
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
         initData();
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if (isLast && scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+                    getMoreData();
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (totalItemCount > 0 && firstVisibleItem + visibleItemCount >= totalItemCount - 1) {
+                    isLast = true;
+                } else {
+                    isLast = false;
+                }
+            }
+        });
     }
 
+    private void getMoreData() {
+        int start = mAdapter.getCount();
+        for (int i = 0 ; i < 20; i++) {
+            mAdapter.add("item " + (start + i));
+        }
+    }
+
+    boolean isLast = false;
+
     private void initData() {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 40; i++) {
             mAdapter.add("item " + i);
         }
     }
